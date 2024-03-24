2b94804afcdd89957f2e6864bc29e6c53b72e99a
GeneralSwiss <nicolas.farrier@gmail.com>
2024-03-23 21:42:48 -0700

    Add flake-compat

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
   outputs = {
