281a66581cccb6601035ede30f94d34d5a0f7346
GeneralSwiss <prime@anbionix.com>
2025-05-24 00:19:11 -0700

    Update LSP configuration and key mappings

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,37 +1,56 @@
 {lib, ...}: {
-  plugins.lsp = {
-    enable = true;
+  lsp = {
+    inlayHints.enable = true;
     keymaps = [
-    {
-    key = "gd";
-    lspBufAction = "definition";
-    }
-    {
-    key = "gD";
-    lspBufAction = "references";
-    }
-    {
-    key = "gt";
-    lspBufAction = "type_definition";
-    }
-    {
-    key = "gi";
-    lspBufAction = "implementation";
-    }
-    {
-    key = "K";
-    lspBufAction = "hover";
-    }
-    {
-    key = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
-    lspBufAction = "<leader>k";
-    }
-    {
-    key = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
-    lspBufAction = "<leader>j";
-    }
+      {
+        key = "gd";
+        lspBufAction = "definition";
+      }
+      {
+        key = "gD";
+        lspBufAction = "references";
+      }
+      {
+        key = "gt";
+        lspBufAction = "type_definition";
+      }
+      {
+        key = "gi";
+        lspBufAction = "implementation";
+      }
+      {
+        key = "K";
+        lspBufAction = "hover";
+      }
+      {
+        key = "<leader>r";
+        lspBufAction = "rename";
+      }
+      {
+        key = "<leader>l";
+        lspBufAction = "format";
+      }
+      {
+        key = "<leader>f";
+        lspBufAction = "code_action";
+      }
+      {
+        action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
+        key = "<leader>k";
+      }
+      {
+        action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
+        key = "<leader>j";
+      }
     ];
     servers = {
+      "*" = {
+        settings = {
+          root_markers = [
+            ".git"
+          ];
+        };
+      };  
       rust_analyzer = {
         enable = true;
       };
--- config/plugins.nix
+++ config/plugins.nix
@@ -7,15 +7,16 @@
     plugins = {
       # web-devicons.enable = true;
       # gitblame.enable = true;
-      # gitgutter.enable = true;
+      gitgutter.enable = true;
       # fugitive.enable = true;
-      # commentary.enable = true;
+      commentary.enable = true;
       # leap.enable = true;
       # fzf-lua.enable = true;
       # indent-blankline.enable = true;
       # treesitter.enable = true;
       # telescope.enable = true;
       # lsp-format.enable = false;
+      lspconfig.enable = true;
     };
   };
 }
